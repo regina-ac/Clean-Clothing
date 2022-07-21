fd1f359b68e0e1e7492e5d382d9da08ef73c0be4
regina alva <regisalvac@gmail.com>
2022-07-21 17:21:57 -0500

    web page

--- Clean Clothing/SearchViewController.swift
+++ Clean Clothing/SearchViewController.swift
@@ -6,13 +6,42 @@
 //
 
 import UIKit
+import WebKit
+
 
 class SearchViewController: UIViewController {
+    
+    private let webView: WKWebView = {
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = preferences
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        return webView
+    }()
 
+    private let url: URL
+    
+    init(url: URL, title: String){
+        self.url = url
+        super.init(nibName: nil, bundle: nil)
+        self.title = title
+    }
+    
+    required init(coder: NSCoder) {
+        ()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(webView)
+        webView.load(URLRequest(url: url))
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
     }
     
 
